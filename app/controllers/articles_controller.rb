909e175fbf661b2a95d15fef7e64e392477d43a0
elizabeth <elizabethlopesramos@gmail.com>
2017-08-17 13:00:57 -0300

    Add create on articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,10 +3,15 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new
   end
 
 private
-  def articles_params
+  def article_params
     params.require(:articles).permit(:title, :text)
   end
 end
