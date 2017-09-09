a40f771022352e0b44989db67c43291eceae673a
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2017-09-09 13:33:42 -0300

    Add some style to layout

--- spec/features/visitor_visit_homepage_spec.rb
+++ spec/features/visitor_visit_homepage_spec.rb
@@ -4,7 +4,7 @@ feature 'Visitor visit homepage' do
   scenario 'successfully' do
     visit root_path
 
-    expect(page).to have_css('h1', text: 'Blog')
-    expect(page).to have_css('h4', text: 'Welcome to this Simple Blog :-)')
+    expect(page).to have_css('h1', text: 'Simple Blog')
+    expect(page).to have_css('h4', text: 'Welcome to this blog!')
   end
 end
